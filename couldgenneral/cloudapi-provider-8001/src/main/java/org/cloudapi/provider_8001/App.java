e36a7bf42e4777a5cc816235704129bee0681924
blavk <blavk@126.com>
2020-12-29 11:01:02 +0800

    change to reactor project

---
+++ couldgenneral/cloudapi-provider-8001/src/main/java/org/cloudapi/provider_8001/App.java
@@ -0,0 +1,13 @@
+package org.cloudapi.provider_8001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ couldgenneral/cloudapi/src/main/java/org/cloudapi/Dept.java
@@ -0,0 +1,35 @@
+package org.cloudapi;
+
+import java.io.Serializable;
+
+public class Dept implements Serializable{
+	
+	private static final long serialVersionUID = 5975243864699322743L;
+
+	private long dno;
+	private String dname;
+	private String dsource;
+	public long getDno() {
+		return dno;
+	}
+	public void setDno(long dno) {
+		this.dno = dno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getDsource() {
+		return dsource;
+	}
+	public void setDsource(String dsource) {
+		this.dsource = dsource;
+	}
+	@Override
+	public String toString() {
+		return "Dept [dno=" + dno + ", dname=" + dname + ", dsource=" + dsource + "]";
+	}
+	
+}
